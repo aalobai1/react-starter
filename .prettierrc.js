554af4a24eee8b29b53fcb835bfa34f89282cc48
aalobai <alinawar2000@gmail.com>
2021-05-03 20:47:34 -0400

    updated some eslint settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,9 @@
 module.exports = {
     semi: true,
     trailingComma: "all",
+    arrowParens: "avoid",
+    singleQuote: true,
+    jsxSingleQuote: true,
+    printWidth: 120,
+    tabWidth: 2
 };
