f54bac1498ab8b6908968fff3468ca3698f348b2
aalobai <alinawar2000@gmail.com>
2021-05-03 20:51:08 -0400

    this is the final eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
       jsx: true, // Allows for the parsing of JSX
     },
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "simple-import-sort"],
   settings: {
     react: {
       version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
@@ -20,6 +20,56 @@ module.exports = {
   },
   // Fine tune rules
   rules: {
+    "@typescript-eslint/camelcase": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      {
+        multiline: {
+          delimiter: "none",
+          requireLast: true,
+        },
+      },
+    ],
+    "@typescript-eslint/no-empty-function": [
+      "error",
+      {
+        allow: ["arrowFunctions"],
+      },
+    ],
+    "@typescript-eslint/no-non-null-assertion": 0,
     "@typescript-eslint/no-var-requires": 0,
+    "react/prop-types": 0,
+    "no-debugger": "error",
+    "no-console": "error",
+    "simple-import-sort/sort": [
+      "error",
+      {
+        groups: [
+          ["^react"],
+          ["^\\u0000"],
+          ["^(@(?!App|Spec))?\\w"],
+          ["^[^.]"],
+          ["^\\."],
+        ],
+      },
+    ],
   },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-member-accessibility": [
+          "error",
+          {
+            accessibility: "no-public",
+            overrides: {
+              parameterProperties: "off",
+            },
+          },
+        ],
+      },
+    },
+  ],
 };
